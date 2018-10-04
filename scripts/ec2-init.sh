14c47e155e7dc8e9401dc67f0f89d6f214a729a2
Gaurav Gupta <g.gupta@hcl.com>
2018-10-03 20:47:35 -0700

    Initial Commit

--- scripts/ec2-init.sh
+++ scripts/ec2-init.sh
@@ -26,28 +26,18 @@ wget -q https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_a
 && rm terraform_0.11.8_linux_amd64.zip
 
 # install install-aws-iam-authenticator
-
 apt-get update
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 
-
 # install kubernetes-client
-
 sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-
-# install install-aws-iam-authenticator
-
-apt-get update
-curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/aws-iam-authenticator
-chmod +x ./aws-iam-authenticator
-sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 
 # clean up
 apt-get clean
